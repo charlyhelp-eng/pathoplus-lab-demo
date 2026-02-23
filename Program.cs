da2a70769effeecec8b21d2b8feec89b244db421
charlyhelp-eng <charly.help@gmail.com>
2026-02-23 16:02:54 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,13 +2,20 @@ using Microsoft.EntityFrameworkCore;
 using PathoPlusLab;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
 app.UseStaticFiles();
 app.UseRouting();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.Run();
+
+// IMPORTANT FOR RAILWAY
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Run($"http://0.0.0.0:{port}");
